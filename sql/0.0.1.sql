286107b73bb83a074bfa7e567d31dcf8d858a9fd
implements1012 <implements1012@gmail.com>
2024-02-09 14:40:17 +0100

    0.0.1

--- sql/0.0.1.sql
+++ sql/0.0.1.sql
@@ -13,3 +13,18 @@ CREATE TABLE `custumer` (
 COLLATE='utf8mb4_general_ci'
 ENGINE=MyISAM
 ;
+
+CREATE TABLE `users` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`firstName` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`lastName` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`email` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`password` LONGTEXT NOT NULL COLLATE 'utf8mb4_general_ci',
+	`permission` INT(10) NOT NULL DEFAULT '1',
+	`createdDate` DATETIME NOT NULL DEFAULT (now()),
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=MyISAM
+;
+
